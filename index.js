1b942ca11cb3f6144a3d01b206814e6c78abc9ff
ms <wsledad@gmail.com>
2026-03-10 20:08:06 +0900

    hard queue update

--- index.js
+++ index.js
@@ -19,11 +19,15 @@ if (!TOKEN) {
   process.exit(1);
 }
 
+// 명령어 채널
 const COMP_COMMAND_CHANNEL = "1480609639757516800";
 const NORMAL_COMMAND_CHANNEL = "1480609696238010378";
 
+// 대기방
 const COMP_WAIT = "1461898802007900281";
+const NORMAL_WAIT = "1461898864012296260";
 
+// 경쟁방 (모든 경쟁 / 빡겜 매칭 이동)
 const COMP_ROOMS = [
   "1462174271932465355",
   "1462174312134869193",
@@ -38,8 +42,7 @@ const COMP_ROOMS = [
   "1475237217499877631"
 ];
 
-const NORMAL_WAIT = "1461898864012296260";
-
+// 일반방
 const NORMAL_ROOMS = [
   "1461897962463432927",
   "1461898016515690566",
@@ -47,6 +50,7 @@ const NORMAL_ROOMS = [
 ];
 
 let compQueue = [];
+let hardQueue = [];
 let normalQueue = [];
 
 client.once("ready", () => {
@@ -57,9 +61,10 @@ client.on("messageCreate", async message => {
 
   if (message.author.bot) return;
 
-  if (message.channel.id === COMP_COMMAND_CHANNEL && message.content === "!경쟁대기") {
+  const member = message.member;
 
-    const member = message.member;
+  // 경쟁대기
+  if (message.channel.id === COMP_COMMAND_CHANNEL && message.content === "!경쟁대기") {
 
     if (!member.voice.channel || member.voice.channel.id !== COMP_WAIT)
       return message.reply("경쟁대기 음성채널에 있어야 합니다.");
@@ -75,9 +80,25 @@ client.on("messageCreate", async message => {
       startMatch(message.guild, compQueue, COMP_ROOMS);
   }
 
-  if (message.channel.id === NORMAL_COMMAND_CHANNEL && message.content === "!일반대기") {
+  // 빡겜대기
+  if (message.channel.id === COMP_COMMAND_CHANNEL && message.content === "!빡겜대기") {
 
-    const member = message.member;
+    if (!member.voice.channel || member.voice.channel.id !== COMP_WAIT)
+      return message.reply("경쟁대기 음성채널에 있어야 합니다.");
+
+    if (hardQueue.includes(member.id))
+      return message.reply("이미 빡겜 대기열에 있습니다.");
+
+    hardQueue.push(member.id);
+
+    message.reply(`빡겜 대기열 (${hardQueue.length}/4)`);
+
+    if (hardQueue.length === 4)
+      startMatch(message.guild, hardQueue, COMP_ROOMS);
+  }
+
+  // 일반대기
+  if (message.channel.id === NORMAL_COMMAND_CHANNEL && message.content === "!일반대기") {
 
     if (!member.voice.channel || member.voice.channel.id !== NORMAL_WAIT)
       return message.reply("일반대기 음성채널에 있어야 합니다.");
@@ -99,18 +120,17 @@ client.on("voiceStateUpdate", (oldState, newState) => {
 
   if (oldState.channelId === COMP_WAIT && newState.channelId !== COMP_WAIT) {
 
-    const index = compQueue.indexOf(oldState.id);
+    const index1 = compQueue.indexOf(oldState.id);
+    if (index1 !== -1) compQueue.splice(index1, 1);
 
-    if (index !== -1)
-      compQueue.splice(index, 1);
+    const index2 = hardQueue.indexOf(oldState.id);
+    if (index2 !== -1) hardQueue.splice(index2, 1);
   }
 
   if (oldState.channelId === NORMAL_WAIT && newState.channelId !== NORMAL_WAIT) {
 
     const index = normalQueue.indexOf(oldState.id);
-
-    if (index !== -1)
-      normalQueue.splice(index, 1);
+    if (index !== -1) normalQueue.splice(index, 1);
   }
 
 });
@@ -120,7 +140,6 @@ async function startMatch(guild, queue, rooms) {
   const available = rooms.find(id => {
 
     const channel = guild.channels.cache.get(id);
-
     return channel && channel.members.size === 0;
 
   });
@@ -128,9 +147,7 @@ async function startMatch(guild, queue, rooms) {
   if (!available) {
 
     console.log("사용 가능한 방 없음");
-
     queue.length = 0;
-
     return;
 
   }
